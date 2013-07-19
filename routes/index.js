6c1ca66146ca0b6bd3b363ccf1e80efc8cb79ce1
zhangyujian <zhangyujian00@gmail.com>
2013-07-19 16:06:51 +0800

    aaa

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,24 @@ function md5(str) {
   return str;
 }
 
+function arrRemoveTail(arr){
+  var new_arr = [];
+  for (var i = 0; i<arr.length - 1; i++){
+    new_arr.push(arr[i]);
+  };
+  return new_arr;
+}
+
+// uploadify 
+exports.upload = function (req, res) {
+  var fileDesc = req.files,
+    imgname = fileDesc.Filedata.name,
+    path = fileDesc.Filedata.path,
+    name = path.replace(config.datapath, ''),
+    imgurl = 'http://localhost:3002/data/img/' + name;
+  res.send(imgurl);
+};
+
 exports.index = function(req, res){
   res.render('index', { 
     title: '首页'
